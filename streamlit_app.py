7e09b81afadba0e63e869b009c7903d79a58b0ae
whonag-git <nag8196@gmail.com>
2025-07-23 10:35:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,3 +52,7 @@ if ingredients_list:
 
         st.success('Your Smoothie is ordered!, ' + name_on_order, icon="✅")
 
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
+
